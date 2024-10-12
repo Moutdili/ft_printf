b0727c46135d1cb296e927aae87255dc07244db5
Idriss Mouhammad Outdili <idriss.outdili@gmail.com>
2024-10-12 05:38:33 +0200

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -13,14 +13,18 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
-#include <stdarg.h>
+#include <stdarg.h>  // Pour les va_list, va_start, etc.
 #include <stddef.h>
 
-int print_char(char c);
+// Déclaration des fonctions
+int ft_printf(const char *format, ...);
+void flush_buffer(char *buffer, int *index);
+int print_char_buffered(char c, char *buffer, int *index);
 int print_str(char *s, char *buffer, int *index);
+int print_number_base(unsigned long n, int base, char *range, char *buffer, int *index);
 int print_unsigned(unsigned int n, char *buffer, int *index);
 int print_hex(unsigned int n, int uppercase, char *buffer, int *index);
-int ft_printf(const char *format, ...);
+int print_pointer(void *ptr, char *buffer, int *index);
+int handle_format(char specifier, va_list args, char *buffer, int *index);
 
 #endif
-
